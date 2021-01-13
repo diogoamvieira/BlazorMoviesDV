31a3a4699097951b4c8f6efab2db2d176b9f3d0a
DiogoAMVieira <diogoamvieira@gmail.com>
2021-01-13 00:13:16 +0000

    Lecture 114 completed

--- BlazorMovies.Components/ExampleJsInterop.cs
+++ BlazorMovies.Components/ExampleJsInterop.cs
@@ -3,9 +3,9 @@ using System.Threading.Tasks;
 
 namespace BlazorMovies.Components
 {
-    public class ExampleJsInterop
+    public static class ExampleJsInterop
     {
-        public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
+        public static ValueTask<string> Prompt(this IJSRuntime jsRuntime, string message)
         {
             // Implemented in exampleJsInterop.js
             return jsRuntime.InvokeAsync<string>(
